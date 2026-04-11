6f31fdc318cb4993c98ba1dd4976610669419b9d
Bryan010206 <deriva010206@gmail.com>
2026-04-11 11:52:36 -0600

    Login funcionando

--- models/usuarios.model.js
+++ models/usuarios.model.js
@@ -6,7 +6,21 @@ const Usuario = {
     getByEmail: (email, cb) => db.query("SELECT * FROM tbl_usuarios WHERE Email = ?", [email], cb),
     create: (data, cb) => db.query("INSERT INTO tbl_usuarios SET ?", data, cb),
     update: (id, data, cb) => db.query("UPDATE tbl_usuarios SET ? WHERE idusuario = ?", [data, id], cb),
-    delete: (id, cb) => db.query("DELETE FROM tbl_usuarios WHERE idusuario = ?", [id], cb)
+    delete: (id, cb) => db.query("DELETE FROM tbl_usuarios WHERE idusuario = ?", [id], cb),
+
+    // Recuperación de contraseña
+    saveResetToken: (Email, token, expiry, cb) => db.query(
+        "UPDATE tbl_usuarios SET reset_token = ?, reset_token_expiry = ? WHERE Email = ?",
+        [token, expiry, Email], cb
+    ),
+    getByResetToken: (token, cb) => db.query(
+        "SELECT idUsuario, Nombres, Apellidos, Email, idRol FROM tbl_usuarios WHERE reset_token = ? AND reset_token_expiry > NOW()",
+        [token], cb
+    ),
+    updatePassword: (idUsuario, hashedPassword, cb) => db.query(
+        "UPDATE tbl_usuarios SET Password_hash = ?, reset_token = NULL, reset_token_expiry = NULL WHERE idUsuario = ?",
+        [hashedPassword, idUsuario], cb
+    ),
 };
 
 module.exports = Usuario;
