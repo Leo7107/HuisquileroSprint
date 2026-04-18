c7cb7a24a5086235ab45b87c9a04d09ed7572729
Bryan010206 <deriva010206@gmail.com>
2026-04-18 02:41:56 -0600

    Refactor checkDuplicado function for readability

--- models/citas.model.js
+++ models/citas.model.js
@@ -23,27 +23,28 @@ const Cita = {
     WHERE c.idcita = ?
   `, [id], cb),
 
-  checkDuplicado: (idDoctor, fecha, hora, excludeId = null, cb) => {
-  if (excludeId) {
-    db.query(
-      `SELECT idCita FROM tbl_citas 
-       WHERE idDoctor = ? AND fecha = ? AND hora = ? 
-       AND estado <> 'CANCELADA' AND idCita <> ?`,
-      [idDoctor, fecha, hora, excludeId], cb
-    );
-  } else {
-    db.query(
-      `SELECT idCita FROM tbl_citas 
-       WHERE idDoctor = ? AND fecha = ? AND hora = ? 
-       AND estado <> 'CANCELADA'`,
-      [idDoctor, fecha, hora], cb
-    );
-  }
-},
+    checkDuplicado: (idDoctor, fecha, hora, excludeId = null, cb) => {
+        if (excludeId) {
+            db.query(
+                `SELECT idCita FROM tbl_citas 
+                 WHERE idDoctor = ? AND fecha = ? AND hora = ? 
+                 AND estado <> 'CANCELADA' AND idCita <> ?`,
+                [idDoctor, fecha, hora, excludeId], cb
+            );
+        } else {
+            db.query(
+                `SELECT idCita FROM tbl_citas 
+                 WHERE idDoctor = ? AND fecha = ? AND hora = ? 
+                 AND estado <> 'CANCELADA'`,
+                [idDoctor, fecha, hora], cb
+            );
+        }
+    },
 
     create: (data, cb) => db.query("INSERT INTO tbl_citas SET ?", data, cb),
     update: (id, data, cb) => db.query("UPDATE tbl_citas SET ? WHERE idcita = ?", [data, id], cb),
-    delete: (id, cb) => db.query("DELETE FROM tbl_citas WHERE idcita = ?", [id], cb)
+    delete: (id, cb) => db.query("DELETE FROM tbl_citas WHERE idcita = ?", [id], cb),
+    completar: (id, cb) => db.query("UPDATE tbl_citas SET estado = 'COMPLETADA' WHERE idCita = ?", [id], cb)
 };
 
 module.exports = Cita;
