4931afdb431b468d4a3cecb8a60857a172f4c579
Bryan010206 <deriva010206@gmail.com>
2026-04-18 03:19:48 -0600

    Add getByUsuarioPaciente method to citas model

--- models/citas.model.js
+++ models/citas.model.js
@@ -23,6 +23,19 @@ const Cita = {
     WHERE c.idcita = ?
   `, [id], cb),
 
+    getByUsuarioPaciente: (idUsuario, cb) => db.query(`
+    SELECT c.*,
+      u_pac.Nombres AS NombrePaciente, u_pac.Apellidos AS ApellidosPaciente,
+      u_doc.Nombres AS NombreDoctor,   u_doc.Apellidos AS ApellidosDoctor
+    FROM tbl_citas c
+    LEFT JOIN tbl_paciente   p     ON c.idPaciente = p.idPaciente
+    LEFT JOIN tbl_usuarios   u_pac ON p.idUsuario  = u_pac.idUsuario
+    LEFT JOIN tbl_doctores   d     ON c.idDoctor   = d.idDoctor
+    LEFT JOIN tbl_usuarios   u_doc ON d.idUsuario  = u_doc.idUsuario
+    WHERE p.idUsuario = ?
+    ORDER BY c.fecha DESC, c.hora DESC
+  `, [idUsuario], cb),
+
     checkDuplicado: (idDoctor, fecha, hora, excludeId = null, cb) => {
         if (excludeId) {
             db.query(
