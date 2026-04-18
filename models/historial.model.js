17d0ec70f1713159659bdd562a67830b537c5585
Bryan010206 <deriva010206@gmail.com>
2026-04-18 10:42:42 -0600

    Add getByPaciente method to historial model

--- models/historial.model.js
+++ models/historial.model.js
@@ -5,7 +5,9 @@ const Historial = {
     getById: (id, cb) => db.query("SELECT * FROM tbl_historial_clinico WHERE idhistorial = ?", [id], cb),
     create: (data, cb) => db.query("INSERT INTO tbl_historial_clinico SET ?", data, cb),
     update: (id, data, cb) => db.query("UPDATE tbl_historial_clinico SET ? WHERE idhistorial = ?", [data, id], cb),
-    delete: (id, cb) => db.query("DELETE FROM tbl_historial_clinico WHERE idhistorial = ?", [id], cb)
+    delete: (id, cb) => db.query("DELETE FROM tbl_historial_clinico WHERE idhistorial = ?", [id], cb),
+    getByPaciente: (idPaciente, cb) => db.query("SELECT * FROM tbl_historial_clinico WHERE idPaciente = ?", [idPaciente], cb)
 };
 
 module.exports = Historial;
+
