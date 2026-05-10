fb374654dba18f34af9748cefc4f1fc871768ec0
Bryan010206 <deriva010206@gmail.com>
2026-05-09 18:30:33 -0600

    Historial de consultas

--- routes/historial.routes.js
+++ routes/historial.routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const historialController = require('../controllers/historial.controller');
+
 router.get('/by-paciente', historialController.getHistorialByPaciente);
+
 router.get('/', historialController.getHistoriales);
+
 router.get('/:id', historialController.getHistorialById);
+
 router.post('/', historialController.createHistorial);
+
 router.put('/:id', historialController.updateHistorial);
+
 router.delete('/:id', historialController.deleteHistorial);
+
 module.exports = router;
