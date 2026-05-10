ad3bb0978a7cec18b9bf41f79042515dd9cc7fcb
Bryan010206 <deriva010206@gmail.com>
2026-05-09 22:34:54 -0600

    HU-12

--- models/consultas.model.js
+++ models/consultas.model.js
@@ -1,11 +1,26 @@
 const db = require("../config/db");
 
 const Consulta = {
-    getAll: (cb) => db.query("SELECT * FROM tbl_consultas", cb),
-    getById: (id, cb) => db.query("SELECT * FROM tbl_consultas WHERE idconsulta = ?", [id], cb),
+    getAll: (cb) => db.query(`
+        SELECT tbl_consultas.*, tbl_usuarios.Nombres AS NombrePaciente, tbl_usuarios.Apellidos AS ApellidosPaciente
+        FROM tbl_consultas
+        LEFT JOIN tbl_historial_clinico ON tbl_consultas.idHistorial = tbl_historial_clinico.idHistorial
+        LEFT JOIN tbl_paciente ON tbl_historial_clinico.idPaciente = tbl_paciente.idPaciente
+        LEFT JOIN tbl_usuarios ON tbl_paciente.idUsuario = tbl_usuarios.idUsuario`, cb),
+
+    getById: (id, cb) => db.query("SELECT * FROM tbl_consultas WHERE idConsulta = ?", [id], cb),
+
+    getByCita: (idCita, cb) => db.query(`
+        SELECT tbl_consultas.*, tbl_usuarios.Nombres AS NombrePaciente, tbl_usuarios.Apellidos AS ApellidosPaciente
+        FROM tbl_consultas
+        LEFT JOIN tbl_historial_clinico ON tbl_consultas.idHistorial = tbl_historial_clinico.idHistorial
+        LEFT JOIN tbl_paciente ON tbl_historial_clinico.idPaciente = tbl_paciente.idPaciente
+        LEFT JOIN tbl_usuarios ON tbl_paciente.idUsuario = tbl_usuarios.idUsuario
+        WHERE tbl_consultas.idCita = ? LIMIT 1`, [idCita], cb),
+
     create: (data, cb) => db.query("INSERT INTO tbl_consultas SET ?", data, cb),
-    update: (id, data, cb) => db.query("UPDATE tbl_consultas SET ? WHERE idconsulta = ?", [data, id], cb),
-    delete: (id, cb) => db.query("DELETE FROM tbl_consultas WHERE idconsulta = ?", [id], cb)
+    update: (id, data, cb) => db.query("UPDATE tbl_consultas SET ? WHERE idConsulta = ?", [data, id], cb),
+    delete: (id, cb) => db.query("DELETE FROM tbl_consultas WHERE idConsulta = ?", [id], cb)
 };
 
 module.exports = Consulta;
